10ea6e33ffa3b509b37c67e015ff42976453acb0
Ryo <shiyanch@gmail.com>
2016-04-16 10:18:12 +0800

    Leetcode 258. Add Digits

---
+++ src/AddDigits/AddDigits.java
@@ -0,0 +1,19 @@
+package AddDigits;
+
+/**
+ * 258. Add Digits
+ * Given a non-negative integer num
+ * repeatedly add all its digits until the result has only one digit.
+ *
+ * For example:
+ * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2.
+ * Since 2 has only one digit, return it.
+ *
+ * Follow up:
+ * Could you do it without any loop/recursion in O(1) runtime?
+ */
+public class AddDigits {
+    public int addDigits(int num) {
+        return num-9*((num-1)/9);
+    }
+}

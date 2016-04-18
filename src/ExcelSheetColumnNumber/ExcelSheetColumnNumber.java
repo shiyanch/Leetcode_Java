08351196025571fc52203d73be9427bdd455d9c7
Ryo <shiyanch@gmail.com>
2016-04-18 19:25:19 +0800

    Leetcode 171. Excel Sheet Column Number

--- src/ExcelSheetColumnNumber/ExcelSheetColumnNumber.java
+++ src/ExcelSheetColumnNumber/ExcelSheetColumnNumber.java
@@ -26,4 +26,12 @@ public class ExcelSheetColumnNumber {
         return sum;
     }
 
+    public int titleToNumber2(String s) {
+        int sum = 0;
+        for(int i=0;i<s.length();i++) {
+            sum = sum * 26 + (s.charAt(i)-'A'+1);
+        }
+
+        return sum;
+    }
 }

5d222dce0e3101da6e68a3f858f3e3ad74681448
Ryo <shiyanch@gmail.com>
2016-09-13 23:47:27 -0400

    Leetcode 122. Best Time to Buy and Sell Stock II

---
+++ src/BestTimeToBuyAndSellStockII/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,23 @@
+package BestTimeToBuyAndSellStockII;
+
+/**
+ * 122. Best Time to Buy and Sell Stock II
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit.
+ * You may complete as many transactions as you like
+ * (ie, buy one and sell one share of the stock multiple times).
+ *
+ * However, you may not engage in multiple transactions at the same time
+ * (ie, you must sell the stock before you buy again).
+ */
+public class BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        for(int i=0;i<prices.length-1;i++) {
+            if (prices[i] < prices[i+1])
+                sum += prices[i+1]-prices[i];
+        }
+        return sum;
+    }
+}

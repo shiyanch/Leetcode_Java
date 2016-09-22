8e52a92cfaf30e28a1e9dd16d3511a644caf594e
Ryo <shiyanch@gmail.com>
2016-09-22 01:17:34 -0400

    Leetcode 50. Pow(x, n)

---
+++ src/PowXN/PowXN.java
@@ -0,0 +1,15 @@
+package PowXN;
+
+/**
+ * 50. Pow(x, n)
+ * Implement pow(x, n).
+ */
+public class PowXN {
+    public double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(n<0){
+            return 1 / (x * myPow(x, -(n + 1)));
+        }
+        return n%2==0 ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+    }
+}

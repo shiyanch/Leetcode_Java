b862fa990d1c9e9e3a92c953af07172cd615b565
Ryo <shiyanch@gmail.com>
2016-09-23 16:05:48 -0400

    Leetcode 63. Unique Paths II

--- src/UniquePathsII/UniquePathsII.java
+++ src/UniquePathsII/UniquePathsII.java
@@ -23,26 +23,18 @@ public class UniquePathsII {
     public int uniquePathWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
 
-        for(int i=0;i<n;i++)
-            if(obstacleGrid[0][i] != 1)
-                dp[0][i] = 1;
-            else
-                break;
+        obstacleGrid[0][0] ^= 1;
+        for(int i=1;i<n;i++)
+            obstacleGrid[0][i] = (obstacleGrid[0][i] == 1)?0:obstacleGrid[0][i-1];
 
-        for(int i=0;i<m;i++)
-            if(obstacleGrid[i][0] != 1)
-                dp[i][0] = 1;
-            else
-                break;
+        for(int i=1;i<m;i++)
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 1)?0:obstacleGrid[i-1][0];
 
         for(int i=1;i<m;i++)
             for(int j=1;j<n;j++)
-                if(obstacleGrid[i][j] != 1)
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                obstacleGrid[i][j] = (obstacleGrid[i][j] == 1)?0:obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
 
-
-        return dp[m-1][n-1];
+        return obstacleGrid[m-1][n-1];
     }
 }

d7dd161e714b3712d76d342eff2e78b158130959
Ryo <shiyanch@gmail.com>
2016-10-14 21:30:21 -0400

    Leetcode 212. Word Search II

--- src/WordSearchII/WordSearchII.java
+++ src/WordSearchII/WordSearchII.java
@@ -4,50 +4,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by shiyanch on 10/14/16.
+ * 212. Word Search II
+ * Given a 2D board and a list of words from the dictionary,
+ * find all words in the board.
+ *
+ * Each word must be constructed from letters of sequentially adjacent cell,
+ * where "adjacent" cells are those horizontally or vertically neighboring.
+ *
+ * The same letter cell may not be used more than once in a word.
  */
 public class WordSearchII {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> res = new ArrayList<>();
-        for(String word:words) {
-            if(exist(board, word))
-                res.add(word);
+        int m = board.length;
+        int n = board[0].length;
+        TrieNode root = buildTrie(words);
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                dfs(board, i, j, root, res);
+            }
         }
 
         return res;
     }
 
-    public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
+    private void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+        char c = board[i][j];
+        if(c == '#' || p.next[c-'a'] == null)
+            return ;
 
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(board[i][j] == word.charAt(0) &&
-                        dfs(board, word, i, j, 0))
-                    return true;
-            }
+        p = p.next[c-'a'];
+        if(p.word != null) {
+            res.add(p.word);
+            p.word = null;
         }
-        return false;
+
+        board[i][j] = '#';
+        if(i > 0) dfs(board, i - 1, j ,p, res);
+        if(j > 0) dfs(board, i, j - 1, p, res);
+        if(i < board.length - 1) dfs(board, i + 1, j, p, res);
+        if(j < board[0].length - 1) dfs(board, i, j + 1, p, res);
+        board[i][j] = c;
     }
 
-    private boolean dfs(char[][] board, String word, int i, int j, int start) {
-        if(start == word.length())
-            return true;
-
-        if(i<0 || i==board.length || j<0 || j==board[0].length)
-            return false;
-
-        if(board[i][j] != word.charAt(start))
-            return false;
-
-        board[i][j] ^= 256;
-        boolean exist = dfs(board, word, i-1, j, start+1) ||
-                dfs(board, word, i+1, j, start+1) ||
-                dfs(board, word, i, j-1, start+1) ||
-                dfs(board, word, i, j+1, start+1);
-
-        board[i][j] ^= 256;
-        return exist;
+    public TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for(String w : words) {
+            TrieNode p = root;
+            for(char c : w.toCharArray()) {
+                int i = c - 'a';
+                if(p.next[i] == null) p.next[i] = new TrieNode();
+                p = p.next[i];
+            }
+            p.word = w;
+        }
+        return root;
     }
+}
+
+class TrieNode {
+    TrieNode[] next = new TrieNode[26];
+    String word;
 }

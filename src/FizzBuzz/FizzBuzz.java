f01426c4466d36d8da0273ad7311954d9ffc2afd
Ryo <shiyanch@gmail.com>
2016-10-14 21:41:56 -0400

    Leetcode 412. Fizz Buzz

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -15,22 +15,20 @@ import java.util.List;
 public class FizzBuzz {
     public List<String> fizzBuzz(int n) {
         List<String> res = new ArrayList<>();
-
         for(int i=1;i<=n;i++) {
-
-            if(i%3==0) {
-                if(i>=5 && i%5==0)
-                    res.add("FizzBuzz");
-                else
-                    res.add("Fizz");
+            if(i%15==0) {
+                res.add("FizzBuzz");
+            }
+            else if(i%3==0) {
+                res.add("Fizz");
             }
             else if(i%5==0) {
                 res.add("Buzz");
             }
-            else
-                res.add(i+"");
+            else {
+                res.add(Integer.toString(i));
+            }
         }
-
         return res;
     }
 }

5eceab005e90d4a8fae3bd939c314492fd919f06
Ryo <shiyanch@gmail.com>
2016-11-08 15:58:55 -0500

    Leetcode 397. Integer Replacement

--- src/IntegerReplacement/IntegerReplacement.java
+++ src/IntegerReplacement/IntegerReplacement.java
@@ -29,4 +29,20 @@ public class IntegerReplacement {
 
         return count;
     }
+
+    // Memory Limit Exceeded
+    public int integerReplacement_dp(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 0;
+
+        for(int i=2;i<=n;i++) {
+            if(i % 2 == 0) {
+                dp[i] = Math.min(dp[i/2]+1, dp[i-1]+1);
+            }
+            else {
+                dp[i] = Math.min(Math.min(dp[i/2]+2, dp[(i+1)/2]+2), dp[i-1]+1);
+            }
+        }
+        return dp[n];
+    }
 }

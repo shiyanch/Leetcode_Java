8eafb887876756309c9eac8d812a95675e450ba3
Ryo <shiyanch@gmail.com>
2016-11-10 19:44:28 -0500

    Leetcode 274. H-Index

---
+++ src/HIndex/HIndex.java
@@ -0,0 +1,65 @@
+package HIndex;
+
+import java.util.Arrays;
+
+/**
+ * 274. H-Index
+ *
+ * Given an array of citations (each citation is a non-negative integer) of a researcher,
+ * write a function to compute the researcher's h-index.
+ *
+ * According to the definition of h-index on Wikipedia:
+ * "A scientist has index h if h of his/her N papers have at least h citations each,
+ * and the other N − h papers have no more than h citations each."
+ *
+ * For example, given citations = [3, 0, 6, 1, 5],
+ * which means the researcher has 5 papers in total and
+ * each of them had received 3, 0, 6, 1, 5 citations respectively.
+ *
+ * Since the researcher has 3 papers with at least 3 citations each
+ * and the remaining two with no more than 3 citations each, his h-index is 3.
+ */
+public class HIndex {
+
+    // 1ms
+    // h最大也就是数组的长度
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int total = 0;
+        int[] arr = new int[n+1];
+        for(int i=0;i<n;i++) {
+            if(citations[i] > n) {
+                arr[n]++;
+            }
+            else {
+                arr[citations[i]]++;
+            }
+        }
+
+        for(int i=n;i>=0;i--) {
+            total += arr[i];
+            if(total >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    // 3ms
+    public int hIndex_2(int[] citations) {
+        Arrays.sort(citations);
+        int count = 0;
+        for(int i=citations.length-1;i>=0;i--) {
+            if(citations[i] > count) {
+                count++;
+            }
+            else return count;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HIndex().hIndex(new int[] {1,2,7,8,9}));
+    }
+}

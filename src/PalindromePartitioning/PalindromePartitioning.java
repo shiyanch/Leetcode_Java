9c7d82a75b6331a4e017b61146cf26edd4bb73bc
Ryo <shiyanch@gmail.com>
2016-11-13 11:43:44 -0500

    Leetcode 131. Palindrome Partitioning

--- src/PalindromePartitioning/PalindromePartitioning.java
+++ src/PalindromePartitioning/PalindromePartitioning.java
@@ -1,8 +1,11 @@
 package PalindromePartitioning;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 131. Palindrome Partitioning
+ *
  * Given a string s, partition s such that every substring of the partition is a palindrome.
  * Return all possible palindrome partitioning of s.
  * For example, given s = "aab",
@@ -14,6 +17,26 @@ import java.util.List;
  */
 public class PalindromePartitioning {
     public List<List<String>> partition(String s) {
-        return null;
+        int len = s.length();
+        List<List<String>>[] result = new List[len+1];
+        result[0] = new ArrayList<>();
+        result[0].add(new ArrayList<>());
+
+        boolean[][] pair = new boolean[len][len];
+        for(int i=0;i<len;i++) {
+            result[i+1] = new ArrayList<>();
+            for(int j=0;j<=i;j++) {
+                if(s.charAt(j) == s.charAt(i) && (i-j<= 1 || pair[j+1][i-1])) {
+                    pair[j][i] = true;
+                    String str = s.substring(j, i+1);
+                    for(List<String> r : result[j]) {
+                        List<String> ri = new ArrayList<>(r);
+                        ri.add(str);
+                        result[i+1].add(ri);
+                    }
+                }
+            }
+        }
+        return result[len];
     }
 }

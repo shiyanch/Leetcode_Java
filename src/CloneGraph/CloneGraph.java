19c94516fdf4aed21468c9b160b1c962b22a33cf
Ryo <shiyanch@gmail.com>
2016-11-13 10:48:45 -0500

    Leetcode 133. Clone Graph

--- src/CloneGraph/CloneGraph.java
+++ src/CloneGraph/CloneGraph.java
@@ -10,8 +10,36 @@ import java.util.*;
  */
 public class CloneGraph {
 
-    // Use Map<originalNode, copyNode> to keep synchronized
+    // DFS
+    private HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        return clone(node);
+    }
+
+    private UndirectedGraphNode clone(UndirectedGraphNode node) {
+        if(node == null) {
+            return null;
+        }
+
+        if(map.containsKey(node.label)) {
+            return map.get(node.label);
+        }
+
+        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
+        map.put(clone.label, clone);
+
+        for(UndirectedGraphNode neighbor : node.neighbors) {
+            clone.neighbors.add(clone(neighbor));
+        }
+
+        return clone;
+    }
+
+
+
+
+    // Use Map<originalNode, copyNode> to keep synchronized
+    public UndirectedGraphNode cloneGraph_BFS(UndirectedGraphNode node) {
         if(node == null) {
             return null;
         }

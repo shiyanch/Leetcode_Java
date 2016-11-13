ddf9e3da11660a76201d2986bb59f80c1be341dd
Ryo <shiyanch@gmail.com>
2016-11-13 17:21:55 -0500

    Leetcode 332. Reconstruct Itinerary

--- src/ReconstructItinerary/ReconstructItinerary.java
+++ src/ReconstructItinerary/ReconstructItinerary.java
@@ -1,7 +1,5 @@
 package ReconstructItinerary;
 
-import ReconstructOriginalDigitsFromEnglish.ReconstructOriginalDigitsFromEnglish;
-
 import java.util.*;
 
 /**
@@ -12,6 +10,14 @@ import java.util.*;
  *
  * All of the tickets belong to a man who departs from JFK.
  * Thus, the itinerary must begin with JFK.
+ *
+ * Note:
+ * 1. If there are multiple valid itineraries,
+ * you should return the itinerary that has the smallest lexical order when read as a single string.
+ * For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+ *
+ * 2. All airports are represented by three capital letters (IATA code).
+ * 3. You may assume all tickets form at least one valid itinerary.
  */
 public class ReconstructItinerary {
     public List<String> findItinerary(String[][] tickets) {

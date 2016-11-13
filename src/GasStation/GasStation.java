65cfd002f275ec3b553787f95ff7250602361cbe
Ryo <shiyanch@gmail.com>
2016-11-12 22:31:26 -0500

    Leetcode 134. Gas Station

--- src/GasStation/GasStation.java
+++ src/GasStation/GasStation.java
@@ -1,10 +1,12 @@
 package GasStation;
 
+
 /**
- * 414. Third Maximum Number
+ * 134. Gas Station
  *
  * There are N gas stations along a circular route,
  * where the amount of gas at station i is gas[i].
+ *
  * You have a car with an unlimited gas tank and
  * it costs cost[i] of gas to travel from station i to its next station (i+1).
  *
@@ -12,36 +14,25 @@ package GasStation;
  * Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
  */
 public class GasStation {
-    public int thirdMax(int[] nums) {
-        if(nums == null || nums.length == 0)
-            return 0;
-        if(nums.length == 1)
-            return nums[0];
-        if(nums.length == 2)
-            return Math.max(nums[0], nums[1]);
 
-        long first = Long.MIN_VALUE;
-        long second = Long.MIN_VALUE;
-        long third = Long.MIN_VALUE;
+    // If car starts at A and can not reach B.
+    // Any station between A and B can not reach B.(B is the first station that A can not reach.)
 
-        for(int i=0;i<nums.length;i++) {
-            if(nums[i] == first || nums[i] == second || nums[i] == third)
-                continue;
+    // If the total number of gas is bigger than the total number of cost.
+    // There must be a solution.
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start = 0;
+        int total = 0;
+        int tank = 0;
 
-            if(nums[i] > first) {
-                third = second;
-                second = first;
-                first = nums[i];
-            }
-            else if(nums[i] > second) {
-                third = second;
-                second = nums[i];
-            }
-            else if(nums[i] > third) {
-                third = nums[i];
+        for(int i=0;i<gas.length;i++) {
+            if((tank += gas[i] - cost[i]) < 0) {
+                start = i+1;
+                total += tank;
+                tank = 0;
             }
         }
 
-        return (int) ((third == Long.MIN_VALUE)?first:third);
+        return (total+tank < 0)?-1:start;
     }
 }

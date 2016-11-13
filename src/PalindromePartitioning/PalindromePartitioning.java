1d5e97a739731ac7b8490a1583cc49d0dc9fdfba
Ryo <shiyanch@gmail.com>
2016-11-13 11:51:20 -0500

    Leetcode 131. Palindrome Partitioning

--- src/PalindromePartitioning/PalindromePartitioning.java
+++ src/PalindromePartitioning/PalindromePartitioning.java
@@ -16,6 +16,8 @@ import java.util.List;
  * ]
  */
 public class PalindromePartitioning {
+
+    // DP solution
     public List<List<String>> partition(String s) {
         int len = s.length();
         List<List<String>>[] result = new List[len+1];
@@ -38,5 +40,37 @@ public class PalindromePartitioning {
             }
         }
         return result[len];
+    }
+
+    // DFS solution
+    public List<List<String>> partitionDFS(String s) {
+        List<List<String>> result = new ArrayList<>();
+        List<String> list = new ArrayList<String>();
+        dfs(s, 0, list, result);
+        return result;
+    }
+
+    private void dfs(String s, int pos, List<String> list, List<List<String>> result) {
+        if(pos == s.length()) {
+            result.add(new ArrayList<>(list));
+        }
+
+        for(int i=pos;i<s.length();i++) {
+            if(isPal(s, pos, i)) {
+                list.add(s.substring(pos, i+1));
+                dfs(s, i+1, list, result);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+
+    private boolean isPal(String s, int low, int high) {
+        while (low < high) {
+            if(s.charAt(low++) != s.charAt(high--)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

1fa0d27fac258b2fc8ffdd24c96041d2b99ba827
Ryo <shiyanch@gmail.com>
2016-11-20 20:06:35 -0500

    Leetcode 459. Repeated Substring Pattern

--- src/RepeatedSubstringPattern/RepeatedSubstringPattern.java
+++ src/RepeatedSubstringPattern/RepeatedSubstringPattern.java
@@ -1,7 +1,13 @@
 package RepeatedSubstringPattern;
 
 /**
- * Created by shiyanch on 11/20/16.
+ * 459. Repeated Substring Pattern
+ *
+ * Given a non-empty string check if it can be constructed
+ * by taking a substring of it and appending multiple copies of the substring together.
+ *
+ * You may assume the given string consists of lowercase English letters only
+ * and its length will not exceed 10000.
  */
 public class RepeatedSubstringPattern {
     public boolean repeatedSubstringPattern(String str) {

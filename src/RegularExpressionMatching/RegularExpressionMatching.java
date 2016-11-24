4d69e1201e03dcf3f32064ed25c119e2d8dc31df
Ryo <shiyanch@gmail.com>
2016-11-23 19:02:20 -0500

    Leetcode 10. Regular Expression Matching

---
+++ src/RegularExpressionMatching/RegularExpressionMatching.java
@@ -0,0 +1,53 @@
+package RegularExpressionMatching;
+
+/**
+ * 10. Regular Expression Matching
+ *
+ * Implement regular expression matching with support for '.' and '*'.
+ */
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        if(s == null || p == null) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i=0; i<p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+
+        for (int i=0; i<s.length(); i++) {
+            for (int j=0; j<p.length(); j++) {
+                if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if(p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    }
+                    else {
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+
+        for (int i=0; i<s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                System.out.print(dp[i + 1][j + 1] + " ");
+            }
+            System.out.println();
+        }
+
+        return dp[s.length()][p.length()];
+    }
+
+    public static void main(String[] args) {
+        String s = "ab";
+        String p = "a*b";
+        System.out.println(new RegularExpressionMatching().isMatch(s, p));
+    }
+}

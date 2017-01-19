e825777c07df10a764c29af0704bfb9bf7fd03fd
Ryo <shiyanch@gmail.com>
2017-01-19 09:26:46 -0500

    Leetcode 50. Pow(x, n)

--- src/PowXN/PowXN.java
+++ src/PowXN/PowXN.java
@@ -8,6 +8,7 @@ public class PowXN {
     public double myPow(double x, int n) {
         if(n==0) return 1;
         if(n<0){
+            // 如果n是Integer.MIN_VALUE，直接-n会溢出
             return 1 / (x * myPow(x, -(n + 1)));
         }
         return n%2==0 ? myPow(x*x, n/2) : x*myPow(x*x, n/2);

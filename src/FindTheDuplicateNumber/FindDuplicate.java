f7e32614d3857f40b3e92cd4eb9872ce361bd385
shiyanch <shiyanch@gmail.com>
2017-07-06 07:56:54 -0400

    Leetcode 287. Find the Duplicate Number

--- src/FindTheDuplicateNumber/FindDuplicate.java
+++ src/FindTheDuplicateNumber/FindDuplicate.java
@@ -2,6 +2,7 @@ package FindTheDuplicateNumber;
 
 /**
  * 287. Find the Duplicate Number
+ *
  * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive)
  * Prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
  *
@@ -33,5 +34,23 @@ public class FindDuplicate {
         }
 
         return c;
+    }
+
+    // O(n)
+    // https://discuss.leetcode.com/topic/25685/java-o-n-time-and-o-1-space-solution-similar-to-find-loop-in-linkedlist
+    public int findDuplicate2(int[] nums) {
+        int n = nums.length;
+        int slow = n;
+        int fast = n;
+        do{
+            slow = nums[slow-1];
+            fast = nums[nums[fast-1]-1];
+        }while(slow != fast);
+        slow = n;
+        while(slow != fast){
+            slow = nums[slow-1];
+            fast = nums[fast-1];
+        }
+        return slow;
     }
 }

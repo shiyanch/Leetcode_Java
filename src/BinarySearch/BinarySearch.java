353f88bc93a3372c5079d5ed08a990990f7b765b
shiyanch <shiyanch@gmail.com>
2020-05-02 13:33:43 -0700

    704. Binary Search

---
+++ src/BinarySearch/BinarySearch.java
@@ -0,0 +1,52 @@
+package BinarySearch;
+
+/**
+ * 704. Binary Search
+ * <p>
+ * Given a sorted (in ascending order) integer array nums of n elements and a target value,
+ * write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * You may assume that all elements in nums are unique.
+ * n will be in the range [1, 10000].
+ * The value of each element in nums will be in the range [-9999, 9999].
+ */
+public class BinarySearch {
+    private static int search(int[] nums, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+
+        if (left == right) {
+            return nums[left] == target ? left : -1;
+        }
+
+        int mid = (left + right) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return search(nums, target, left, mid - 1);
+        } else {
+            return search(nums, target, mid + 1, right);
+        }
+    }
+
+    public int search(int[] nums, int target) {
+        return search(nums, target, 0, nums.length - 1);
+    }
+}
